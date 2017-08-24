c9514092ac0a58f144d733ca4c923193513b896c
williamsonlogan <williamson.logan@gmail.com>
2017-08-23 19:38:49 -0700

    LoadCSV Class

--- Assets/Scripts/LoadCSV.cs
+++ Assets/Scripts/LoadCSV.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class LoadCSV : MonoBehaviour {
+	Card Load(string file)
+	{
+		Card returnCard = new Card ();
+		string loadedString;
+		StreamReader sr = new StreamReader (file);
+		loadedString = sr.ReadLine ();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		string[] split = loadedString.Split (",");
+
+		for(int i = 0; i < split.GetLength(); i++){
+			switch(i)
+			{
+			case 0:
+				returnCard.cardTitle = split [i];
+				break;
+			case 1:
+				returnCard.staminaCost = split [i];
+				break;
+			case 2:
+				returnCard.damage = split [i];
+				break;
+			case 3:
+				returnCard.charges = split [i];
+				break;
+			case 4:
+				returnCard.cardType = split [i];
+				break;
+			case 5:
+				returnCard.cardImage = split [i];
+				break;
+			default:
+				Debug.Log ("THIS SHOULDNT HAPPEN!!!! BAD!!");
+				break;
+		}
 	}
 }
