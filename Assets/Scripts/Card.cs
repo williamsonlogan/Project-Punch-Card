8363c532f0ce8da2b26fe8317c9692e06082fa29
Hunter McCullough <huntermccullough56@gmail.com>
2017-08-31 17:29:50 -0700

    Didn't make it in the last commit

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,33 +7,43 @@ using System.IO;
 public enum PunchType { Speed = 0, Power = 1 }
 
 public class Card : MonoBehaviour {
+
 	public string cardTitle; //Name of the card
-	public int staminaCost = 1; //Cost of stamina to play card
-	public int damage = 1; //Damge dealt if connect
-	public int charges = 1; //Amount of times a card can be used before it is destroyed
+	public int staminaCost; //Cost of stamina to play card
+	public int damage; //Damge dealt if connect
+	public int charges; //Amount of times a card can be used before it is destroyed
 	public PunchType cardType; // Type of card
 	public Sprite cardImage; //Image to be displayed on card
-	public Image UIImage; //Image that exists in physical space
+
+    // UI Variables
+    public Image UIImage; //Image that exists in physical space
 	public Text UITitle; //Text that exists in physical space
 	public Text UICost; //^^
 
-	// Use this for initialization
-	void Start () {
-		Card tempcard = LoadCSV.Load("./Assets/Data/testdeck.csv");
-		cardTitle = tempcard.cardTitle;
-		staminaCost = tempcard.staminaCost;
-		damage = tempcard.damage;
-		charges = tempcard.charges;
-		cardType = tempcard.cardType;
-		cardImage = tempcard.cardImage;
+    public void Start()
+    {
+        UIImage.sprite = cardImage;
+        UITitle.text = cardTitle;
+        UICost.text = staminaCost.ToString();
+    }
 
-		UIImage.sprite = cardImage;
-		UITitle.text = cardTitle;
-		UICost.text = "" + staminaCost;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Card(string _title, int _cost, int _damage, int _charges, PunchType _type, Sprite _sprite)
+    {
+        cardTitle = _title;
+        staminaCost = _cost;
+        damage = _damage;
+        charges = _charges;
+        cardType = _type;
+        cardImage = _sprite;
+    }
+
+    public void Set(Card card)
+    {
+        cardTitle = card.cardTitle;
+        staminaCost = card.staminaCost;
+        damage = card.damage;
+        charges = card.charges;
+        cardType = card.cardType;
+        cardImage = card.cardImage;
+    }
 }
